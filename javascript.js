fce4fb74cc9972ea33fb0386b25685d63ee8b8bc
enzojnardone <101941591+enzojnardone@users.noreply.github.com>
2022-04-25 10:40:36 +0200

    itaration 6

--- javascript.js
+++ javascript.js
@@ -115,10 +115,43 @@ console.log(avenger)
 // Muestra en consola el último personaje del array
 // const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
 
-const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+let rickAndMortyCharacters = ["Rick", "Beth", "Jerry"]; //Se cambia con "const" por "let" para poder manipular la variable"
 rickAndMortyCharacters.push("Morty", "Summer")
 console.log(rickAndMortyCharacters[4])
 
 // 1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
 // const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 
+// 1.6 Elimina el segundo elemento del array y muestra el array por consola.
+// const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+// Iteración #5: Condicionales
+// En base al código siguiente, muestra los mensajes correctos por consola.
+
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+if (number2 / number1 === 2) {
+    console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (number1 !== number2) {
+    console.log("number1 es estrictamente distinto a number2");
+}
+
+if (number3 != number1) {
+    console.log("number3 es distinto number1");
+}
+
+if (number3 * 5 == number1) {
+    console.log("number3 por 5 es igual a number1");
+}
+
+if (number3 * 5 == number1 && number1 * 2 == number2) {
+    console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+  }
+
+  if (number2 / 2 == number1 || number1 / 5 == number3) {
+      console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+  }
